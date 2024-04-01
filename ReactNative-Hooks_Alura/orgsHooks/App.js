f0a0ab44445ef0818ca2f000c1e6aff43fb3f605
Haroldo H. M. Mayworm <haroldomayworm@gmail.com>
2024-04-01 10:37:26 -0300

    Feat: Create React Native project

---
+++ ReactNative-Hooks_Alura/orgsHooks/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>First files</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
